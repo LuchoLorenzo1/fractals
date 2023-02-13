a985e485382336d52608b1104da3896ff2db1bc6
Luciano Lorenzo <lucianoalorenzo1@gmail.com>
2023-02-13 15:03:01 -0300

    general changes

--- mandelbrot/main.js
+++ mandelbrot/main.js
@@ -1,8 +1,16 @@
-const canvas = document.getElementById('tree')
+const canvas = document.getElementById('fractal')
 const ctx = canvas.getContext('2d')
 const width = canvas.width
 const height = canvas.height
 
+X0 = -2
+X1 = 1
+
+Y0 = -1.5
+Y1 = 1.5
+
+dx = Math.abs(X1 - X0) / width
+dy = Math.abs(Y1 - Y0) / height
 
 const drawFractal = (intervalA, intervalB, fractal) => {
 	const imageData = ctx.createImageData(width, height)
@@ -20,9 +28,9 @@ const drawFractal = (intervalA, intervalB, fractal) => {
 			alpha = fractal(X0 + i * dx, Y0 - j * dy)
 
 			let index = 4 * (i + j * width)
-			if(0 < alpha < 0.5){
-				imageData.data[index + 0] = 0
-				imageData.data[index + 1] = 0
+			if (0 < alpha < 0.5) {
+				imageData.data[index + 0] = alpha
+				imageData.data[index + 1] = alpha
 				imageData.data[index + 2] = 255
 			} else {
 				imageData.data[index + 0] = 200 - alpha * 120
@@ -30,12 +38,11 @@ const drawFractal = (intervalA, intervalB, fractal) => {
 				imageData.data[index + 2] = alpha * 100
 			}
 
-			imageData.data[index + 3] = alpha * 200
+			imageData.data[index + 3] = alpha * 255
 		}
 	}
 	ctx.putImageData(imageData, 0, 0)
 }
-
 
 const mandelbrot = (ca, cb) => {
 	iterations = 100
@@ -77,14 +84,32 @@ const julia = (x, y, f) => {
 	return 1
 }
 
-const f = (a, b) => {
-	return [a * a - b * b + 0.279, 2 * a * b]
+const f = (a, b, ca, cb) => {
+	return [a * a - b * b + ca, 2 * a * b + cb]
 }
 
 const g = (a, b) => {
-		n = Math.exp(a)
-		return [n * Math.cos(b) - 0.65, n * Math.sin(b)]
+	n = Math.exp(a)
+	return [n * Math.cos(b) - 0.65, n * Math.sin(b)]
 }
+
+// mousedown = false
+// canvas.addEventListener('mousedown', (e) => {
+// 	mousedown = true
+// })
+// canvas.addEventListener('mouseup', (e) => {
+// 	mousedown = false
+// })
+// canvas.addEventListener('mousemove', (e) => {
+// 	if (mousedown) {
+// 		drawFractal([-2, 1], [-1.5, 1.5], (x, y) => julia(x, y, (x, y) => f(x, y, e.x * dx + X0, Y0 - e.y * dy)))
+// 		console.log(e.x * dx + X0, e.y * dy + Y0)
+// 	}
+// })
+
+// canvas.addEventListener('click', (e) => {
+// 	drawFractal([-2, 1], [-1.5, 1.5], (x, y) => julia(x, y, (x, y) => f(x, y, e.x * dx + X0, Y0 - e.y * dy)))
+// })
 
 // drawFractal([-10, 10], [-10, 10], (x, y) => julia(x, y, g))
 // drawFractal([-1, 1], [-1.5, 1.5], (x, y) => julia(x, y, f))
